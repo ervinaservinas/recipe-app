aab05c5d00ab686bda5a1fc45696ab34da5fec38
Ervinas <84942742+ervinaservinas@users.noreply.github.com>
2023-03-08 21:24:51 +0200

    added mongoDB connection with env

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config();
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.MONGODB_ACCESS_KEY);
 
 app.listen(3001, () => console.log("Server started"));
